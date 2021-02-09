bc537e9d49785401a5bbe7d59b24d918e888564d
PaulaDolado <78765611+PaulaDolado@users.noreply.github.com>
2021-02-09 11:12:21 +0100

    Create 02-exemple.sh

---
+++ 02-exemple.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#@paula ASIX M01 
+#febrer 2021
+#
+# ------------------
+echo '$*:' $*
+echo '$@:' $@
+echo '$#:' $#
+echo '$0:' $0
+echo '$1:' $1
+echo '$2:' $2
+echo '$3:' $3
+echo '$4:' $4
+echo '$5:' $5
+echo '$6:' $6
+echo '$7:' $7
+echo '$8:' $8
+echo '$9:' $9
+echo '$10:' ${10}
+echo '$11:' ${11}
+nom='paula'
+echo "${nom} dolado"
+exit 0
