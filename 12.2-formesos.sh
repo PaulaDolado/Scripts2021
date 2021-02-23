30c0e42e4e2df77e71be69bb55792972f8c09ea0
PaulaDolado <78765611+PaulaDolado@users.noreply.github.com>
2021-02-23 12:33:40 +0100

    Create 12.2-formesos.sh

---
+++ 12.2-formesos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#@paula ASIX M01-ISO
+#Febrer 2021
+# prog mes[...]
+#   validar 1 o més arguments
+#   per cada més validar més [1,12]
+#   per cada més diu el número de dies
+#------------------------------------------
