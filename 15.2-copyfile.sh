28acc9c39a7c0a084aa0ca2ce67dc30b4b6828c8
PaulaDolado <78765611+PaulaDolado@users.noreply.github.com>
2021-02-24 12:34:49 +0100

    Create 15.2-copyfile.sh

---
+++ 15.2-copyfile.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# @paula ASIX M01
+# Curs 2020-2021
+# Febrer 2021
+# copyfile  file[...] dirdestí
+        # validar arg
+        # validar dir-->ultim arg
+        # per cada file -->llistar
+        # elif else
+# -------------------------------------
