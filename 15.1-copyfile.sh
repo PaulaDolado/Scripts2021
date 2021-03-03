1bb687a41e97a2c1351c7ccfe82caf7323def954
PaulaDolado <78765611+PaulaDolado@users.noreply.github.com>
2021-03-03 12:42:33 +0100

    Update 15.1-copyfile.sh

--- 15.1-copyfile.sh
+++ 15.1-copyfile.sh
@@ -29,3 +29,30 @@ echo $# | sed -r 's/^.* $//' #ens elimina tot fins ultim
 echo $# | cut -d' ' -f1-$(($#-1))  
 echo $# | cut -d' ' -f$#
 #ultim argument
+
+#---------------------------------------------
+#copyfile file a dir destí
+  #validem els arguments
+ERR_ARG=1
+ERR_NOEXIST=2
+if [ $# -ne 2 ] #han de ser 2 arg degut a que el file + dir 
+then
+    echo "Error: nºarguments incorrecte"
+    echo "Usage: $0 arg insuficient"
+    exit $ERR_ARG
+fi
+  #validem que es un fitxer + aquest existeix
+file=$1
+if [ ! -e $file ]
+then
+  echo"Error: $file no existeix"
+  exit $ERR_NOEXIST
+fi
+  #validem que es un directori
+dir=$2
+if [! -d $dir]
+then
+  echo"Error: $dir no es un directori"
+  exit $NO_EXIST
+
+cp -r $file /desti
