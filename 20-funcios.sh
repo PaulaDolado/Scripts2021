a0d740d55af4cbf6bc78ea6975b9f16ce9e62e00
PaulaDolado <78765611+PaulaDolado@users.noreply.github.com>
2021-03-24 08:18:27 +0100

    Update 20-funcios.sh

--- 20-funcios.sh
+++ 20-funcios.sh
@@ -3,26 +3,100 @@
 # Març de 2021
 # exemple de funcions
 #------------------------------
-ERR_ARG=1
 
+
+
+
+
+
+
+# 2) -------------------------------------------------- 
+# showUserGecos(login) 
+# mostrar un a un els camps amb label # del GECOS 
+function showUserGecos(){
+ ERR_NARGS=1
+ ERR_NOLOGIN=2
+ OK=0
+ #1) validar arg
+ if [ $# -ne 1 ]; then
+   echo "Error: num args incorrecte"
+   echo "usage: $0 login"
+   return $ERR_NARGS
+ fi
+ #2) validar existeix
+ login=$1
+ line=""
+ line=$(grep "^$login:" /etc/passwd 2> /dev/null)
+ if [ -z "$line" ]; then
+   echo "Error: login $login inexist"
+   echo "usage: $0 login"
+   return $ERR_NOLOGIN
+ fi
+ #3) mostrar
+ gecos=$(echo $line | cut -d: -f5)
+ name=$(echo $gecos | cut -d, -f1)
+ office=$(echo $gecos | cut -d, -f2)
+ phoneOffi=$(echo $gecos | cut -d, -f3)
+ phoneHome=$(echo $gecos | cut -d, -f4)
+ echo "name: $name"
+ echo "office: $office"
+ echo "office phone: $phoneOffi"
+ echo "home phone: $phoneHome"
+ return 0
+}
+#---------------------------------------------------
 #mostrar camp a camp user
+ function showUser(){
+ ERR_NARGS=1
+ ERR_NOLOGIN=2
+ OK=0
+ #1) validar arg
+ if [ $# -ne 1 ]; then
+   echo "Error: num args incorrecte"
+   echo "usage: $0 login"
+   return $ERR_NARGS
+ fi
+ 
+ #2) validar existeix login
+ login=$1
+ line=""
+ line=$(grep "^$login:" /etc/passwd 2> /dev/null)
+ if [ -z "$line" ]; then
+   echo "Error: login $login inexist"
+   echo "usage: $0 login"
+   return $ERR_NOLOGIN
+ fi
+ 
+ #3) mostrar
+ uid=$(echo $line | cut -d: -f3)
+ gid=$(echo $line | cut -d: -f4)
+ gecos=$(echo $line | cut -d: -f5)
+ home=$(echo $line | cut -d: -f6)
+ shell=$(echo $line | cut -d: -f7)
+ echo "login: $login"
+ echo "uid: $uid"
+ echo "gid: $gid"
+ echo "home: $home"
+ echo "shell: $shell"
+ return 0
+}
 
-function showuser(){
-  #validem que rep1 arg
-if [ $# -ne 1 ]
-then
-  echo "Error: número args no vàlid"
-  echo "usarge: $0 dir"
-  exit $ERR_ARG
-fi
-  #validem existeix user
-user=$1
-if [ user -  ]
+function hola(){
+  echo "hola"
+  return 0
+}
 
-#mostrem camp a camp
-while read
+function dia(){
+  date
+  return 0
+}
 
-#---------------------------
+function suma(){
+  echo $(($1+$2))
+  return 0	
+}
+
+#----------------------------------------
 #Prova classe
 
 function suma(){
